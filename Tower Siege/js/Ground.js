5d64220b4e5b0bf9635d0258a31ea6df7fa55b1e
prachiArora05 <80327613+prachiArora05@users.noreply.github.com>
2021-10-17 15:48:51 +0530

    Tower Siege by Prachi

---
+++ Tower Siege/js/Ground.js
@@ -0,0 +1,13 @@
+class Ground {
+  constructor() {
+    this.ground = Bodies.rectangle(450, 390, 900, 20, {isStatic:true});
+    World.add(world, this.ground);
+  }
+  
+  display() {
+    noStroke();
+    fill(188, 67 ,67);
+    rectMode(CENTER);
+    rect(this.ground.position.x, this.ground.position.y, 900, 20);
+  }
+}
